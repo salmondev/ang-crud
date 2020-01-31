188b57f2ba6a4ef59db4b5ea7b099d6b8b2f61fe
panurut chinakul <panurut.army@hotmail.com>
2020-01-31 14:55:33 +0700

    Production

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'CRUD';
+  title = 'app';
+  head = 'CRUD stack = Angular + Spring Boot + mongoDB';
+  dev = 'panurut';
 }
